1f473ed4e3711659a2f54c456a40f551ee63a35a
fartimusprime286 <blardonijorge.a@gmail.com>
2024-12-19 23:10:08 -0500

    terminate

--- core/util.py
+++ core/util.py
@@ -1,5 +1,17 @@
+import sys
+
+from core import logging
+from core.updating import UpdateHandler
+
+
 def get_or_none[T](lst: list[T], idx: int) -> (T | None):
     try:
         return lst[idx]
     except IndexError:
         return None
+
+def terminate():
+    UpdateHandler.instance.quit()
+    UpdateHandler.instance.join()
+    logging.force_flush()
+    sys.exit(0)
